f7b52b0fcf233f3c38c3b941bc6f7e26442b5492
Trai_nc <trainguyenchi30@gmail.com>
2026-02-25 17:43:18 +0700

    add env.js

---
+++ View/env.js
@@ -0,0 +1,10 @@
+const ENV = {
+    // Base URL cho các API BE
+    API_BASE_URL: "https://api.nguyenchitrai.id.vn",
+    
+    // Base URL cho MCP Server 
+    MCP_SERVER_URL: "https://mcp-server-9zc1.onrender.com",
+
+    // Base URL cho custom PDF API
+    PDF_API_BASE_URL: "http://localhost:8631"
+};
